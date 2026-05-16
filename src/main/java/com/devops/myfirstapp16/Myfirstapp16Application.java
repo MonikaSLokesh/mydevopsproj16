28d37578843927cb6832c0e524390ca46cedb679
Monika <moni24052706@gmail.com>
2026-05-16 11:26:05 +0530

    updated

--- src/main/java/com/devops/myfirstapp16/Myfirstapp16Application.java
+++ src/main/java/com/devops/myfirstapp16/Myfirstapp16Application.java
@@ -8,6 +8,8 @@ public class Myfirstapp16Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Myfirstapp16Application.class, args);
+		System.out.println("MyDevops Project");
 	}
+
 
 }
