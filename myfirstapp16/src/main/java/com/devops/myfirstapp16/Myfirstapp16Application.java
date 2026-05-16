b7aa636f31df1fd2a749e2183c6860cc91a00316
Monika <moni24052706@gmail.com>
2026-05-16 09:09:55 +0530

    Initial commit

---
+++ myfirstapp16/src/main/java/com/devops/myfirstapp16/Myfirstapp16Application.java
@@ -0,0 +1,13 @@
+package com.devops.myfirstapp16;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myfirstapp16Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myfirstapp16Application.class, args);
+	}
+
+}
---
+++ myfirstapp16/src/test/java/com/devops/myfirstapp16/Myfirstapp16ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.myfirstapp16;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Myfirstapp16ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
